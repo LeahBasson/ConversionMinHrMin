d7626514a557837b6b52681914244b04e7415460
LeahBasson <leahbasson54@gmail.com>
2025-03-04 23:57:22 +0200

    :tada:

---
+++ ConversionMinHrMin/src/conversionminhrmin/ConversionMinHrMin.java
@@ -0,0 +1,15 @@
+
+package conversionminhrmin;
+
+
+public class ConversionMinHrMin {
+
+   
+    public static void main(String[] args) {
+        double min = 210;
+        double hrsAndMin = min/60; 
+        
+        System.out.println("Conversion minutes to hours and remaining minutes: " + hrsAndMin);
+    }
+    
+}
